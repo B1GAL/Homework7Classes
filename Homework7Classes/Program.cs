ed5c5d0de7cee3aa3482c90e2d03214d9f041d90
Alex Upton <alexander99w@ou.edu>
2021-04-26 00:44:29 -0500

    almost done

--- Homework7Classes/Program.cs
+++ Homework7Classes/Program.cs
@@ -6,7 +6,28 @@ namespace Homework7Classes
     {
         static void Main(string[] args)
         {
+            Patient p1 = new Patient();
+            p1.FirstName = "Alex";
+            p1.MidInitial = 'W';
+            p1.LastName = "Upton";
+            p1.ID = 0;
+            p1.FluVaccineYN = false;
+            p1.Weights.Add(DateTime.Now, 180.6);
+            p1.Weights.Add(DateTime.Now.AddDays(-450), 155);
+            p1.Weights.Add(DateTime.Now.AddDays(-780), 190);
+            p1.AppointmentHistory.Add("Yearly Checkup");
+            p1.AppointmentHistory.Add("Physical");
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            p1.AdministerVaccine();
+            Console.WriteLine(p1);
+            Console.WriteLine();
+            p1.AdministerVaccine();
+            Console.WriteLine(p1);
             
+
+
         }
     }
 }
