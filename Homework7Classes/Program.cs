288cf8efa1b6547dc055629b89e4eb97044fa459
Alex Upton <alexander99w@ou.edu>
2021-04-25 19:41:24 -0500

    Add project files.

---
+++ Homework7Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework7Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
